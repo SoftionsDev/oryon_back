482b7069f114ec4564a53f2b512f4621dd581a92
Sebastian <jsebas791@gmail.com>
2023-11-10 09:59:56 -0500

    feat: commercial master services

---
+++ users/commercial_urls.py
@@ -0,0 +1,14 @@
+from django.urls import re_path
+from rest_framework.routers import DefaultRouter
+from users.views import CommercialViewSet
+
+router = DefaultRouter()
+router.register(r'', CommercialViewSet)
+
+urlpatterns = [
+    re_path(
+        r'(?P<user__email>[\w\.-]+@[\w\.-]+\.\w+)$',
+        CommercialViewSet.as_view({'get': 'retrieve'}),
+        name='commercial-detail'
+    )
+] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.models import User
-from users.serializers import UserSerializer, TokenSerializer
+from users.serializers import UserSerializer, TokenSerializer, CommercialSerializer
 from utils.permissions import IsAdmin, IsManager
+from users.models import User, Commercial
 
 
 class UserViewSet(ModelViewSet):
@@ -16,3 +16,24 @@ class UserViewSet(ModelViewSet):
 
 class UserToken(TokenObtainPairView):
     serializer_class = TokenSerializer
+
+
+class CommercialViewSet(ModelViewSet):
+
+    model = Commercial
+    serializer_class = CommercialSerializer
+    queryset = Commercial.objects.select_related('user', 'manager').all()
+    permission_classes = [IsAdmin|IsManager]
+    lookup_field = 'user__email'
+
+    def get_queryset(self):
+        if self.request.user.is_admin:
+            return Commercial.objects.select_related(
+                'user', 'manager'
+            ).all()
+
+        # it has to be a manager to retrieve other commercials
+        manager_id = self.request.user.code
+        return Commercial.objects.filter(
+            manager_id=manager_id
+        ).select_related('user', 'manager')
